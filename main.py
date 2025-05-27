48383733daa3ca681bb60829bd90881af122b4b3
illdoCc <103tony50505@gmail.com>
2025-05-27 10:46:14 +0800

    complete player's code

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import psycopg2
 import pandas as pd
 import json
 import os
 from datetime import datetime
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import traceback
 
 app = FastAPI()
 
@@ -30,7 +32,7 @@ DB_NAME = "postgres"
 DB_USER = "postgres"
 DB_PASSWORD = "admin"
 
-@app.post("/start")
+@app.get("/start")
 def clear_and_load_stock():
     try:
         # 連線資料庫
@@ -57,7 +59,7 @@ def clear_and_load_stock():
         
         # 刷新stock table
         stock_init_prices = [] # return value
-        holdings = {}
+        holdings = []
         for id, stock_symbol in enumerate(ids):
             # 取30天
             start_date = datetime.strptime('2021/2/25', '%Y/%m/%d')
@@ -73,7 +75,12 @@ def clear_and_load_stock():
                 "name": f"{stock_symbol} {stock_name.get(str(stock_symbol), '')}",
                 "price": prices_list[0]
             })
-            holdings[id] = 0
+            holdings.append({
+                "stock_id": id,
+                "stock_name": f"{stock_symbol} {stock_name.get(str(stock_symbol), '')}",
+                "count": 0
+            })
+            # holdings[stock_symbol] = 0
             # 插入資料
             cur.execute(
                 "INSERT INTO stock (id, history_price, name, stock_symbol) VALUES (%s, %s, %s, %s);",
@@ -88,6 +95,7 @@ def clear_and_load_stock():
                 "INSERT INTO history (day, user_name, holdings, cash) VALUES (%s, %s, %s, %s);",
                 (0, user, json.dumps(holdings), 5000000.0)
             )
+        
             histories.append({
                 "day": 1,
                 "user_name": user,
@@ -98,7 +106,7 @@ def clear_and_load_stock():
         cur.close()
         conn.close()
         return {
-            "stock": stock_init_prices, 
+            "stocks": stock_init_prices, 
             "histories": histories
         }
     except Exception as e:
@@ -107,8 +115,11 @@ def clear_and_load_stock():
 
     
 
-@app.post("/advance")
-def next_day_game():
+class Portfolio(BaseModel):
+    stocks: list
+
+@app.post("/advance/{day}")
+def next_day_game(day, portfolio: Portfolio):
     try:
         # 連線資料庫
         conn = psycopg2.connect(
@@ -121,50 +132,101 @@ def next_day_game():
         conn.autocommit = True
         cur = conn.cursor()
 
-
-        # model_list = ['arima', 'RNN', 'lstm', 'RF', 'XGB']
+        latest_stock_price = get_latest_stock_price(cur, day)
+        histories = []
         
-        rows = cur.execute("SELECT * FROM history").fetchall()
+        # model_list = ['arima', 'RNN', 'lstm', 'RF', 'XGB']
+        # rows = cur.execute
+        cur.execute("SELECT * FROM history")
+        rows = cur.fetchall()
         for row in rows:
             day, user_name, holdings, cash = row
             day += 1
-
-            latest_stock_price = get_latest_stock_price(cur, day)
             
-            if(user_name == 'no_buy'):
+            if user_name == 'no_buy':
                 cur.execute(
                     "INSERT INTO history (day, user_name, holdings, cash) VALUES (%s, %s, %s, %s);",
                     (day, user_name, json.dumps(holdings), 5000000.0)
                 )
-            elif(user_name != 'player'):
-                model = user_name.split('_')[1]
-                predict_prices = model_predict_price(model, '2021/2/25', day)
-                holdings, cash = model_buy_or_sell(predict_prices, holdings, cash)
-                cur.execute(
-                    "INSERT INTO history (day, user_name, holdings, cash) VALUES (%s, %s, %s, %s);",
-                    (day, user_name, json.dumps(holdings), cash)
-                )
+            elif user_name != 'player':
+                pass
+                # model = user_name.split('_')[1]
+                # predict_prices = model_predict_price(model, '2021/2/25', day)
+                # holdings, cash = model_buy_or_sell(predict_prices, holdings, cash)
+                # cur.execute(
+                #     "INSERT INTO history (day, user_name, holdings, cash) VALUES (%s, %s, %s, %s);",
+                #     (day, user_name, json.dumps(holdings), cash)
+                # )
+            elif user_name == 'player':
+                histories.append(player_portfolio(cur, day, portfolio))
+            
+        for history in histories:
+            cur.execute(
+                "INSERT INTO history (day, user_name, holdings, cash) VALUES (%s, %s, %s, %s);",
+                (history["day"], history["user_name"], json.dumps(history["holdings"]), history["cash"])
+            )
                 
-
-
-
+ 
         cur.close()
         conn.close()
         return {
-            "latest stock price": latest_stock_price, 
-            "all Histories": "" 
+            "stocks": latest_stock_price, 
+            "histories": histories
         }
     except Exception as e:
-        return {"error": str(e)}
-    
+        return {"error": traceback.format_exc()}
+# end of next_day_game
+
+
 # 取得當天股價
 def get_latest_stock_price(cur, day):
-    latest_stock_price = {}
-    rows = cur.execute("SELECT * FROM stock").fetchall()
+    day = int(day)
+    latest_stock_price = []
+
+    cur.execute("SELECT * FROM stock")
+    rows = cur.fetchall()
     for row in rows:
-        id, history_prices = row
-        latest_stock_price[id] = history_prices[day]
+        id, history_prices, name, stock_symbol = row
+        latest_stock_price.append({
+            "id": id,
+            "name": f"{stock_symbol} {name}",
+            "price": history_prices[day]
+        })
+
     return latest_stock_price
+# end of get_latest_stock_price
+
+def player_portfolio(cur, day, portfolio):
+    portfolio = portfolio.stocks
+    day = int(day)
+    cur.execute("SELECT * FROM history WHERE user_name = %s", ("player",))
+    player_info = cur.fetchone()
+    _, user_name, holdings, cash = player_info
+    holdings = json.loads(holdings)
+
+    for portfo in portfolio:
+        cur.execute("SELECT * FROM stock WHERE id = %s", (int(portfo["id"]),))
+        row = cur.fetchone()
+        id, history_prices, name, stock_symbol = row
+        buy_or_sell_price = history_prices[day - 1]
+        buy_or_sell_amount = portfo["count"]
+
+        holdings[id]["count"] += buy_or_sell_amount
+        if holdings[id]["count"] < 0:
+            raise HTTPException(status_code=400, detail="You do not have enough shares to sell.")
+        cash += (history_prices[day] - buy_or_sell_price) * buy_or_sell_amount
+        if cash < 0:
+            cash = 0
+        
+    return {
+        "day": day,
+        "user_name": "player",
+        "holdings": holdings,
+        "cash": cash
+    }
+
+        
+
 
 # 取得模型預測價格
 price_label_df = pd.read_csv('price_labeled.csv', encoding='big5')
@@ -178,11 +240,8 @@ def model_predict_price(model, start_date, day):
     for stock in stock_models:
         stock_id = stock[:4]
 
-
-
-
     return predict_prices
-
+# end of model_predict_price
 
 
 # 依據預測價格決定買與賣多少
