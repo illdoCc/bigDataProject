58888a767f8ddaf3d8f395480555feb5b2743087
illdoCc <103tony50505@gmail.com>
2025-06-01 16:22:30 +0800

    all in tsmc

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import traceback
 from dotenv import load_dotenv
-import algorithm
 import os
 
 app = FastAPI()
@@ -38,14 +37,13 @@ DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "admin")
 
 
-
 with open('predict_price/price_labels.json', 'r', encoding='UTF-8') as f:
     price_labels = json.load(f)
 
 with open('portfolio.json', 'r', encoding='UTF-8') as f:
     model_portfolio = json.load(f)
 
-name_map = {'player': 'player', 'al_lstm': 'player(LSTM)','al_arima': 'player(ARIMA)','al_RNN': 'player(RNN)','al_RF': 'player(Random Forest)','al_XGB': 'player(XGBoost)','no_buy': 'player(No Buy)'}
+name_map = {'player': 'player', 'al_lstm': 'player(LSTM)','al_arima': 'player(ARIMA)','al_RNN': 'player(RNN)','al_RF': 'player(Random Forest)','al_XGB': 'player(XGBoost)','no_buy': 'player(No Buy)', 'all_in_tsmc': 'player(All in TSMC)'}
 
 @app.get("/start")
 def clear_and_load_stock():
@@ -104,18 +102,23 @@ def clear_and_load_stock():
 
 
         histories = [] # return value
-        user_list = ['player', 'al_lstm', 'al_arima', 'al_RNN', 'al_RF', 'al_XGB', 'no_buy']
+        user_list = ['player', 'al_lstm', 'al_arima', 'al_RNN', 'al_RF', 'al_XGB', 'no_buy', 'all_in_tsmc']
         for user in user_list:
+            cash = 5000000
+            if user == 'all_in_tsmc':
+                holdings[22]['count'] = 10080
+                cash = 320
+
             cur.execute(
                 "INSERT INTO history (day, user_name, holdings, cash) VALUES (%s, %s, %s, %s);",
-                (0, user, json.dumps(holdings), 5000000.0)
+                (0, user, json.dumps(holdings), cash)
             )
 
             histories.append({
                 "day": 1,
                 "user_name": name_map[user],
                 "holdings": holdings,
-                "cash": 5000000
+                "cash": cash
             })
 
         cur.close()
@@ -126,9 +129,7 @@ def clear_and_load_stock():
         }
     except Exception as e:
         return {"error": str(e)}
-
-
-
+    
 
 class Portfolio(BaseModel):
     stocks: list
@@ -151,8 +152,6 @@ def next_day_game(day:int, portfolio: Portfolio):
         latest_stock_price = get_latest_stock_price(cur, day)
         histories = []
 
-        # model_list = ['arima', 'RNN', 'lstm', 'RF', 'XGB']
-
         # only updates the history with history of previous day
         # -2 because database starts from day 0, but frontend starts from day 1
         cur.execute("SELECT * FROM history WHERE day=%s",(day-2, ))
@@ -161,11 +160,12 @@ def next_day_game(day:int, portfolio: Portfolio):
         for row in rows:
             day, user_name, holdings, cash = row
             day += 1
-            # print(day)
-            if user_name == 'no_buy':
+            #print(day)
+
+            if user_name == 'no_buy' or user_name == 'all_in_tsmc':
                 histories.append({
                     "day": day,
-                    "user_name": 'no_buy',
+                    "user_name": user_name,
                     "holdings": json.loads(holdings),
                     "cash": cash
                 })
@@ -186,10 +186,8 @@ def next_day_game(day:int, portfolio: Portfolio):
             "INSERT INTO history (day, user_name, holdings, cash) VALUES (%s, %s, %s, %s);",
             records
         )
-        # print(records)
 
         for history in histories:
-            # print(history[user_name])
             history['user_name'] = name_map[history['user_name']]
 
         cur.close()
@@ -199,8 +197,8 @@ def next_day_game(day:int, portfolio: Portfolio):
             "histories": histories
         }
     except Exception as e:
-        tb = traceback.format_exc()
-        return {"status": "error", "message": str(e), "traceback": tb}
+        # tb = traceback.format_exc()
+        # return {"status": "error", "message": str(e), "traceback": tb}
         raise HTTPException(status_code=400, detail=str(e))
 # end of next_day_game
 
@@ -222,6 +220,7 @@ def get_latest_stock_price(cur, day):
 
     return latest_stock_price
 # end of get_latest_stock_price
+
 
 def player_portfolio(cur, day:int, portfolio):
     portfolio = portfolio.stocks
@@ -261,17 +260,7 @@ def player_portfolio(cur, day:int, portfolio):
 
 # 依據預測價格決定買與賣多少
 def model_buy_or_sell(day, model, holdings, cash):
-    # current_price = {stock_id: values[day] for stock_id, values in price_labels.items() if len(values) > day}
     portfolio = model_portfolio[str(day)][model]
-
-    
-    
-    # portfolio = algorithm.optimize_portfolio_dict(
-    #     current_price=current_price, 
-    #     predicted_prices=predicted_prices,
-    #     current_holding_array=holdings,
-    #     total_capital=cash*0.1
-    # )
     
     for stock, count in portfolio.items():
         history_price = price_labels[stock][day]  # 取得當天該股票價格
